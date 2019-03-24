d727281b799c1b2e1f42e83555d8cf6da582c69c
wuyingfeng <785184093@qq.com>
2019-03-24 18:49:27 +0800

    aside

--- src/components/asiderMenu.vue
+++ src/components/asiderMenu.vue
@@ -1,25 +1,24 @@
 <script src="../../../../node/panel/src/pages/sourceManage/sourceManage.js"></script>
 <template>
   <div class="asiderMenu" @click="handleClick">
-    asider
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-          <el-menu-item :index="item.id" v-for="item in getPageMenu">
-            <!--<i class="el-icon-setting"></i>-->
-            <span slot="title">{{item.name}}</span>
-          </el-menu-item>
-        </el-menu>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item :index="item.id" v-for="item in getPageMenu">
+        <!--<i class="el-icon-setting"></i>-->
+        <span slot="title">{{item.name}}</span>
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 
 <script>
-  import {mapState,mapGetters} from 'vuex';
+  import {mapState, mapGetters} from 'vuex';
 
   export default {
     name: "asider-menu",
